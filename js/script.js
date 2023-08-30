9e047e7ac17efbc0f2bbc53879d58ce6a205e658
“Sebastian <impactotenisok@gmail.com>
2023-08-30 16:20:00 -0300

    Se agregó tipografia en visor, se dió estilo a botones de funcion, se modificó btn cancel se arregló permanencia de resultado en pantalla

--- js/script.js
+++ js/script.js
@@ -6,13 +6,18 @@ let number1=0;
 let number2=0;
 let operacion;
 let resultado;
+let borrarpantalla="false"
 
 
 // Agrega el evento de clic a cada botón
 numberButtons.forEach(Element => {
     Element.addEventListener("click", function() {
+        if (borrarpantalla=="true"){
+            visor.innerHTML=""
+        }
+        borrarpantalla="false"
         const number = Element.textContent;
-       visor.innerHTML+=number    
+        visor.innerHTML+=number    
     });
 });
 
@@ -20,8 +25,14 @@ operaciones.forEach(Element =>{
     Element.addEventListener("click", function(){
         const operaciones = Element.textContent;
         number1=parseFloat(visor.innerHTML);
-        visor.innerHTML="";
+        borrarpantalla="true"
         operacion=operaciones
+        if (operacion== "Borrar"){
+            visor.innerHTML=""
+            number1=0;
+            number2=0;
+            console.log(operacion)
+        }
     })
 })
 
@@ -34,7 +45,6 @@ calcular.addEventListener("click", function(){
     }else {
 
     if (operacion== "+"){
-        console.log(operacion)
         resultado= number1 + number2
     }
     if (operacion== "-"){
@@ -47,11 +57,6 @@ calcular.addEventListener("click", function(){
         resultado= number1 / number2
     }
 
-    if (operacion== "cancelar"){
-        visor.innerHTML=""
-        number1=0;
-        number2=0;
-    }
     visor.innerHTML=resultado
 }
 }) 
